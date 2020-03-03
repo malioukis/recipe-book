332f70d1c8864e2ee89b8905d07861a491fed51b
Malioukis Spyros <s.malioukis@gmail.com>
2020-03-03 11:43:33 +0200

    fixed header, created model, rough data interpolation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    // calling constructor
+    new Recipe('Test Recipe', 'test desc', 'https://media.gettyimages.com/photos/herbs-picture-id637769910?b=1&k=6&m=637769910&s=612x612&w=0&h=dV9qigGyhtsmhPEa2y7zjjzRNiZhfOi_TN2XotMMwMU=')
+  ];
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+// Blueprint
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  // to instantiate with the keyword new
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
