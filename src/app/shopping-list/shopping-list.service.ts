35a4c14559e5cf5013c57b4dca1d1333e0196c4f
Malioukis Spyros <s.malioukis@gmail.com>
2020-03-04 14:12:49 +0200

    shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,25 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Ingredient1', 2),
+    new Ingredient('Ingredient2', 34),
+    new Ingredient('Ingredient3', 45),
+    new Ingredient('Ingredient4', 34)
+  ];
 
   constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+
+  }
 }
